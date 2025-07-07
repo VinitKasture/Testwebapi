6951adc9cd4c16cb9a84ca6033e90be3b731011f
VinitKasture <97528616+VinitKasture@users.noreply.github.com>
2025-07-07 12:51:17 +0530

    Add files via upload

---
+++ TWA/Controllers/CItyController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TestWebApi.Controllers
+{
+    [Route("api/cities")]
+    public class CItyController : ControllerBase
+    {
+
+        [HttpGet]
+        public JsonResult GetCities()
+        {
+            return new JsonResult(CitiesDataStore.current.cities);
+        }
+
+        [HttpGet("{id}")]
+        public JsonResult GetCity(int id)
+        {
+            return new JsonResult(CitiesDataStore.current.cities.FirstOrDefault((city) => city.Id == id));
+        }
+    }
+}
---
+++ TWA/DbContext/CityInfoContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestWebApi.Entities;
+
+namespace TestWebApi
+{
+    public class CityInfoContext : DbContext
+    {
+        public CityInfoContext(DbContextOptions<CityInfoContext> options)
+        : base(options)
+        {
+        }
+
+        public DbSet<City> Cities { get; set; }
+        public DbSet<PointOfInterest> PointOfInterests { get; set; }
+    }
+
+}
